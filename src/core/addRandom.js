82ceb425d2d58fed92f945b2e7698b7eeb1f4c79
mattia richetto <mattia.richetto@gmail.com>
2020-04-05 15:40:49 -0700

    check if the grid is full

--- src/core/addRandom.js
+++ src/core/addRandom.js
@@ -3,6 +3,26 @@ function getRandomInt(max) {
 }
 
 function addRandom(afterMove) {
+  if (
+    afterMove[0][0] !== null &&
+    afterMove[0][1] !== null &&
+    afterMove[0][2] !== null &&
+    afterMove[0][3] !== null &&
+    afterMove[1][0] !== null &&
+    afterMove[1][1] !== null &&
+    afterMove[1][2] !== null &&
+    afterMove[1][3] !== null &&
+    afterMove[2][0] !== null &&
+    afterMove[2][1] !== null &&
+    afterMove[2][2] !== null &&
+    afterMove[2][3] !== null &&
+    afterMove[3][0] !== null &&
+    afterMove[3][1] !== null &&
+    afterMove[3][2] !== null &&
+    afterMove[3][3] !== null
+  ) {
+    return afterMove;
+  }
   const x = getRandomInt(4);
   const y = getRandomInt(4);
   if (afterMove[x][y] === null) {
