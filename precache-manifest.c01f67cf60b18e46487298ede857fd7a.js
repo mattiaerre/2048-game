2f58cf13db782774854f77ded86ed1d0b1e82735
mattia richetto <mattia.richetto@gmail.com>
2020-04-05 16:17:51 -0700

    Updates

---
+++ precache-manifest.c01f67cf60b18e46487298ede857fd7a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae85bdccc7375cdbd7c3397be415e72a",
+    "url": "/2048-game/index.html"
+  },
+  {
+    "revision": "6efc36daefcd7fe7cbcb",
+    "url": "/2048-game/static/css/main.3b8e3bf6.chunk.css"
+  },
+  {
+    "revision": "c35af37840ba0f62557d",
+    "url": "/2048-game/static/js/2.22927932.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/2048-game/static/js/2.22927932.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6efc36daefcd7fe7cbcb",
+    "url": "/2048-game/static/js/main.bd8d3dba.chunk.js"
+  },
+  {
+    "revision": "9966a607dcffced6e722",
+    "url": "/2048-game/static/js/runtime-main.499214e0.js"
+  }
+]);
